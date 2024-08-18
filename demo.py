718c3b489b14d35101235fee4b19fa700eacf51f
mertcobanov <mertcobanovv@gmail.com>
2024-08-19 00:03:03 +0300

    feat: Optimize keyframe detection with lazy loading for images

--- demo.py
+++ demo.py
@@ -1,25 +1,21 @@
 import os
+
 from keyframe.framer import Framer
 
 
 def main():
-    video_path = "asset/short.mp4"
-    output_dir = "output"
-
-    # Choose the keyframe detection algorithm
-    algorithm = "mean_squared_error"  # You can choose from "pixelwise", "background_subtraction", "optical_flow", etc.
-
     # Initialize the Framer class
     framer = Framer()
 
-    print(f"Processing video: {video_path} using {algorithm} algorithm")
-    keyframes, frame_indices = framer.process_video(
-        video_path, algorithm=algorithm, output_dir=output_dir
-    )
+    video_path = "asset/short.mp4"
+    algorithm = "pixelwise"  # [pixelwise, background_subtraction, optical_flow, histogram_comparison, sift_matching, mean_squared_error, phase_correlation]
+    output_dir = f"output/{algorithm}_keyframes"
 
-    # Output the results
-    print(f"Detected {len(keyframes)} keyframes.")
-    print(f"Keyframes saved to: {os.path.abspath(output_dir)}")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    print(f"Processing video with '{algorithm}' algorithm...")
+    framer.process_video(video_path, algorithm, output_dir)
 
 
 if __name__ == "__main__":
